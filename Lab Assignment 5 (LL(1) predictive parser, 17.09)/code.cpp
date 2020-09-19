87d1579a56e506909d4a0f039ae6e751c4de3f15
Ashish Dasari <ashishdasari148@gmail.com>
2020-09-19 16:56:54 +0530

    till now

--- Lab Assignment 5 (LL(1) predictive parser, 17.09)/code.cpp
+++ Lab Assignment 5 (LL(1) predictive parser, 17.09)/code.cpp
@@ -6,6 +6,30 @@ typedef struct symbols{
     int id;
     bool is_terminal;
 }symbols;
+void findfirst(vector<symbols> list_of_symbols, int firstof, unordered_map<int,vector<vector<int>>> productions, vector<unordered_set<int>> *first, unordered_map<string,int> name_to_id){
+    // cout << list_of_symbols[firstof].name << endl;
+    if(list_of_symbols[firstof].is_terminal){
+        first->at(firstof).insert(list_of_symbols[firstof].id);
+        return;
+    }
+    for(int i=0;i<productions[firstof].size();i++){
+        unordered_set<int> tempset;
+        tempset.insert(name_to_id["epsilon"]);
+        for(int position = 0; position<productions[firstof][i].size()&&tempset.find(name_to_id["epsilon"])!=tempset.end();position++){
+            tempset.erase(tempset.find(name_to_id["epsilon"]));
+            // cout << " First size() : " << first->at(productions[firstof][i][position]).size() << endl;
+            if(first->at(productions[firstof][i][position]).size()==0){
+                findfirst(list_of_symbols, productions[firstof][i][position], productions, first, name_to_id);
+            }
+            tempset.insert(first->at(productions[firstof][i][position]).begin(), first->at(productions[firstof][i][position]).end());
+        }
+        first->at(firstof).insert(tempset.begin(), tempset.end());
+    }
+    // cout << endl << list_of_symbols[firstof].name << " = ";
+    // for(auto j=first->at(firstof).begin();j!=first->at(firstof).end();j++){
+    //     cout << list_of_symbols[*j].name << " ";
+    // }
+}
 int main() {
     int number_of_symbols;
     unordered_map<string,int> name_to_id;
@@ -28,6 +52,11 @@ int main() {
     x.id = list_of_symbols.size();
     x.is_terminal = true;
     name_to_id["epsilon"] = list_of_symbols.size();
+    list_of_symbols.push_back(x);
+    x.name = "eof";
+    x.id = list_of_symbols.size();
+    x.is_terminal = true;
+    name_to_id["eof"] = list_of_symbols.size();
     list_of_symbols.push_back(x);
     // for(int i=0;i<number_of_symbols;i++){
     //     cout << list_of_symbols[i].name << " ";
@@ -262,7 +291,68 @@ int main() {
             return 0;
         }
     }
-
+    string start_s;
+    int start_i;
+    cout << "Specify the start symbol: ";
+    cin >> start_s;
+    start_i = name_to_id[start_s];
+    vector<unordered_set<int>> first(list_of_symbols.size(), unordered_set<int>());
+    vector<unordered_set<int>> follow(list_of_symbols.size(), unordered_set<int>());
+    for(int i=0;i<list_of_symbols.size();i++){
+        if(list_of_symbols[i].is_terminal){
+            first[i].insert(list_of_symbols[i].id);
+        }
+        else if(first[i].size()==0){
+            findfirst(list_of_symbols, i, productions, &first, name_to_id);
+            // cout << " First size() : " << first[productions[i][0][0]].size() << endl;
+            // break;
+        }
+        cout << endl << "FIRST(" << list_of_symbols[i].name << ") : ";
+        for(auto j=first[i].begin();j!=first[i].end();j++){
+            cout << list_of_symbols[*j].name << " ";
+        }
+    }
+    cout << endl;
+    follow[start_i].insert(name_to_id["eof"]);
+    int follow_size_prev[list_of_symbols.size()];
+    memset(follow_size_prev, 0, sizeof(follow_size_prev));
+    do{
+        doer = false;
+        for(auto k=productions.begin();k!=productions.end();k++){
+            for(int i=0;i<k->second.size();i++){
+                for(int j=0;j<k->second[i].size();j++){
+                    if(!list_of_symbols[k->second[i][j]].is_terminal){
+                        unordered_set<int> tempset;
+                        tempset.insert(name_to_id["epsilon"]);
+                        for(int position = j+1; position<k->second[i].size()&&tempset.find(name_to_id["epsilon"])!=tempset.end();position++){
+                            tempset.erase(tempset.find(name_to_id["epsilon"]));
+                            tempset.insert(first[k->second[i][position]].begin(), first[k->second[i][position]].end());
+                        }
+                        if(tempset.find(name_to_id["epsilon"])!=tempset.end() || j+1>=k->second[i].size()){
+                            tempset.erase(tempset.find(name_to_id["epsilon"]));
+                            follow[k->second[i][j]].insert(follow[k->first].begin(),follow[k->first].end());
+                        }
+                        follow[k->second[i][j]].insert(tempset.begin(), tempset.end());
+                    }
+                }
+            }
+        }
+        for(int i=0;i<list_of_symbols.size();i++){
+            if(follow_size_prev[i]!=follow[i].size()){
+                follow_size_prev[i] = follow[i].size();
+                doer = true;
+            }
+        }
+    }while(doer);
+    for(int i=0;i<list_of_symbols.size();i++){
+        if(list_of_symbols[i].is_terminal)
+            continue;
+        cout << endl << "FOLLOW(" << list_of_symbols[i].name << ") : ";
+        for(auto j=follow[i].begin();j!=follow[i].end();j++){
+            cout << list_of_symbols[*j].name << " ";
+        }
+    }
+    cout << endl;
     // vector<int> tokens;
     // int token;
     // yyin=fopen("inputfile.txt","r");
@@ -270,6 +360,7 @@ int main() {
     //     cout << token << "\t" << yytext << endl;
     //     tokens.push_back(token);
     // }
+    // tokens.push_back(name_to_id["eof"]);
     // for(int i=0;i<tokens.size();i++)
     //     cout << tokens[i] << " ";
     // return 0;
